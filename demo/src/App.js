d628fe91189d55d23f7a18be79972b0c73374cae
William L <w-lindvall@outlook.com>
2020-05-26 03:03:03 -0700

    update demo

--- demo/src/App.js
+++ demo/src/App.js
@@ -4,13 +4,22 @@ import RubberSlider from './react-rubber-slider'
 import './react-rubber-slider/styles.css'
 
 export const App = () => {
-  const [value, setValue] = useState(0.5)
+  const [value, setValue] = useState(50)
 
   return (
     <div className="app">
       <h1 className="title">rubber-slider</h1>
-      <RubberSlider width={250} value={value} onChange={setValue} />
-      <p className="rating-value">{(value * 100).toFixed(0)}</p>
+      <RubberSlider
+        width={250}
+        value={value}
+        onChange={setValue}
+        min={1}
+        max={100}
+        onDragStart={(...args) => console.log('drag start', args)}
+        onDrag={(...args) => console.log('drag', args)}
+        onDragEnd={(...args) => console.log('drag end', args)}
+      />
+      <p className="rating-value">{value}</p>
     </div>
   )
 }
