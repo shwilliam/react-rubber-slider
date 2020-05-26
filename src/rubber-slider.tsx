5cf99e6f20fb9ee6ecbb4af0c0e97bc1a72074f3
William L <w-lindvall@outlook.com>
2020-05-25 23:57:46 -0700

    refactor d3 imports

--- src/rubber-slider.tsx
+++ src/rubber-slider.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react'
 import {SliderInput, SliderTrack, SliderHandle} from '@reach/slider'
-import * as d3 from 'd3' // TODO: import deps from d3 microlibraries
+import {easeElastic, event, select, line, curveCatmullRom, drag} from 'd3'
 
 export const RubberSlider = ({
   value = 0,
@@ -8,7 +8,7 @@ export const RubberSlider = ({
   width = 200,
   height = 100,
   id = 'rubber-slider',
-  easing = d3.easeElastic,
+  easing = easeElastic,
 }) => {
   const points = [
     [0, height / 2],
@@ -17,22 +17,20 @@ export const RubberSlider = ({
   ]
 
   const onDrag = () => {
-    d3.event.subject[0] = Math.max(0, Math.min(width, d3.event.x))
-    d3.event.subject[1] = Math.max(0, Math.min(height, d3.event.y))
+    event.subject[0] = Math.max(0, Math.min(width, event.x))
+    event.subject[1] = Math.max(0, Math.min(height, event.y))
     update()
   }
-  const getDragTarget = () => d3.event.sourceEvent.target.__data__
+  const getDragTarget = () => event.sourceEvent.target.__data__
   const onDragEnd = () => {
     points[1][1] = height / 2
     update()
   }
   const draw = () => {
-    const svg = d3
-      .select(`#${id}-container`)
+    const svg = select(`#${id}-container`)
       .attr('viewBox', `0, 0, ${width}, ${height}`)
       .call(
-        d3
-          .drag()
+        drag()
           .subject(getDragTarget)
           .on('drag', onDrag)
           .on('end', onDragEnd) as any,
@@ -48,13 +46,13 @@ export const RubberSlider = ({
       .call(update)
   }
   const update = () => {
-    const svg = d3.select('svg')
+    const svg = select('svg')
     svg
       .select('path')
       .transition()
       .duration(500)
       .ease(easing)
-      .attr('d', d3.line().curve(d3.curveCatmullRom) as any)
+      .attr('d', line().curve(curveCatmullRom) as any)
 
     const circle = svg.selectAll('g').data([points[1]], d => d as string)
     onChange(points[1][0] / width)
