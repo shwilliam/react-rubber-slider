a0d7f5bffbba7eff47594971fe906c15fd8ef50c
William L <w-lindvall@outlook.com>
2020-05-25 17:38:58 -0700

    check built docs

---
+++ docs/assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"1":"External module","64":"Function"},"rows":[{"id":0,"kind":1,"name":"\"rubber-slider\"","url":"modules/_rubber_slider_.html","classes":"tsd-kind-external-module"},{"id":1,"kind":64,"name":"RubberSlider","url":"modules/_rubber_slider_.html#rubberslider","classes":"tsd-kind-function tsd-parent-kind-external-module","parent":"\"rubber-slider\""},{"id":2,"kind":1,"name":"\"index\"","url":"modules/_index_.html","classes":"tsd-kind-external-module"}]};
