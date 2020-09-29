d77e111dd2c95d39793f6911190d46d02067e7a1
simsimjae <simsimjae@naver.com>
2020-09-29 16:18:16 +0900

    add prettierrc

--- src/rubber-slider.d.ts
+++ src/rubber-slider.d.ts
@@ -1,24 +1,24 @@
 export interface IRubberSliderProps {
-  id?: string
-  name?: string
-  className?: string
-  value?: number
-  onChange?: (value: number) => void
-  width?: number
-  height?: number
-  max?: number
-  min?: number
-  step?: number
-  easeFunction?: (normalizedTime: number) => number
-  easeDuration?: number
-  onDragStart?: (position: TSliderPos) => void
-  onDrag?: (position: TSliderPos) => void
-  onDragEnd?: (position: TSliderPos) => void
-  style?: IStyleProp
+	id?: string
+	name?: string
+	className?: string
+	value?: number
+	onChange?: (value: number) => void
+	width?: number
+	height?: number
+	max?: number
+	min?: number
+	step?: number
+	easeFunction?: (normalizedTime: number) => number
+	easeDuration?: number
+	onDragStart?: (position: TSliderPos) => void
+	onDrag?: (position: TSliderPos) => void
+	onDragEnd?: (position: TSliderPos) => void
+	style?: IStyleProp
 }
 
 export interface IStyleProp {
-  [styleKey: string]: any
+	[styleKey: string]: any
 }
 
 export type TSliderPos = [TSliderValue, TSliderHorizontalOffset]
--- src/utils.ts
+++ src/utils.ts
@@ -1,28 +1,19 @@
-import {event} from 'd3'
+import { event } from 'd3'
 
 export const getNormalizedOffset = (value: number, max: number) =>
-  value < max / 2 ? 1 - value / (max / 2) : (-1 * (value - max / 2)) / (max / 2)
+	value < max / 2 ? 1 - value / (max / 2) : (-1 * (value - max / 2)) / (max / 2)
 
-export const getSteppedValue = (value: number, step: number) =>
-  value - (value % step)
+export const getSteppedValue = (value: number, step: number) => value - (value % step)
 
 export const getEventCoords = (width: number, height: number) => [
-  Math.max(0, Math.min(width, event.x)),
-  Math.max(0, Math.min(height, event.y)),
+	Math.max(0, Math.min(width, event.x)),
+	Math.max(0, Math.min(height, event.y)),
 ]
 
 export const getDragTarget = () => event.sourceEvent.target.__data__
 
-export const getValueFromPos = (
-  pos: number,
-  posMax: number,
-  min: number,
-  max: number,
-) => (pos / posMax) * (max - min) + min
+export const getValueFromPos = (pos: number, posMax: number, min: number, max: number) =>
+	(pos / posMax) * (max - min) + min
 
-export const getPosFromValue = (
-  value: number,
-  maxPos: number,
-  min: number,
-  max: number,
-) => ((value - min) / (max - min)) * maxPos
+export const getPosFromValue = (value: number, maxPos: number, min: number, max: number) =>
+	((value - min) / (max - min)) * maxPos
